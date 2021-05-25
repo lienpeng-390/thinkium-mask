2832fa511cd7766acd7214bc653b0c50f11263b6
lienpeng-390 <lep0390@163.com>
2021-05-25 11:48:07 +0800

    添加thinkium链

---
+++ app/scripts/first-time-state.js
@@ -0,0 +1,32 @@
+/**
+ * @typedef {Object} FirstTimeState
+ * @property {Object} config Initial configuration parameters
+ * @property {Object} NetworkController Network controller state
+ */
+
+/**
+ * @type {FirstTimeState}
+ */
+const initialState = {
+  config: {},
+  PreferencesController: {
+    frequentRpcListDetail: [
+      {
+        rpcUrl: 'http://localhost:8545',
+        chainId: '0x539',
+        ticker: 'ETH',
+        nickname: 'Localhost 8545',
+        rpcPrefs: {},
+      },
+      {
+        rpcUrl: 'http://rpctest1.thinkium.org/',
+        chainId: '0x2711',
+        ticker: 'TKM',
+        nickname: 'thinkium1',
+        rpcPrefs: {},
+      },
+    ],
+  },
+};
+
+export default initialState;
